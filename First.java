31046b15d82df7ba4b37c25a54e1928433e7c0d6
armena124 <128395631+armena124@users.noreply.github.com>
2023-04-27 11:40:45 -0400

    Update First.java

--- First.java
+++ First.java
@@ -1,37 +1,44 @@
-import java.awt.event.*;  //import necessary packages
-import javax.swing.*; 
-import java.awt.Color;
+//import necessary packages
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 public class first
 {
     public static void main(String[] args)
     {
+        //set up frame
         JFrame main_frame = new JFrame("Check for integer");  
-        main_frame.setSize(1000, 500); 
-
+        main_frame.setSize(500, 500); 
+        
+        //GUI elements 
         JLabel prompt = new JLabel("Enter an integer: ");
-        prompt.setBounds(350, 100, 200, 25);
+        prompt.setBounds(100, 100, 200, 25);
         main_frame.add(prompt);
 
         JLabel right_input = new JLabel("You entered an integer");
-        right_input.setBounds(450, 250, 120, 25);
+        right_input.setBounds(210, 250, 120, 25);
         main_frame.add(right_input);
         right_input.setVisible(false);
 
         JLabel wrong_input = new JLabel("Please Enter an integer");
-        wrong_input.setBounds(450, 250, 150, 25);
+        wrong_input.setBounds(190, 250, 150, 25);
         main_frame.add(wrong_input);
         wrong_input.setVisible(false);
 
         JTextField user_input = new JTextField();
-        user_input.setBounds(460, 100, 200, 25);
+        user_input.setBounds(200, 100, 200, 25);
 
         JButton verify_button = new JButton("Verify"); 
-        verify_button.setBounds(550, 150, 100, 40); 
-        verify_button.setBackground(Color.GREEN);
+        verify_button.setBounds(100, 150, 100, 40); 
+        
         verify_button.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
             {
+                //Verify if input is integer
                 String user_string = user_input.getText(); 
                 try
                 {
@@ -53,8 +60,7 @@ public class first
             }
         });
         JButton exit_button = new JButton("Exit");
-        exit_button.setBounds(400, 150, 100, 40);
-        exit_button.setBackground(Color.RED);
+        exit_button.setBounds(300, 150, 100, 40);
         exit_button.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
